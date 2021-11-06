33663559c775929827fe9a3af19f34f6fa107d7c
Nightowl286 <79215414+nightowl286@users.noreply.github.com>
2021-11-06 21:18:05 +0000

    Added the initial MVVM framework.
    Setup the basic Caliburn.Micro bootstrapper

---
+++ src/HusicWindowsUI/Bootstrapper.cs
@@ -0,0 +1,26 @@
+﻿using Caliburn.Micro;
+using Husic.Windows.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Husic.Windows
+{
+   internal class Bootstrapper : BootstrapperBase
+   {
+      public Bootstrapper()
+      {
+         Initialize();
+      }
+
+      #region Overrides
+      protected override void OnStartup(object sender, StartupEventArgs e)
+      {
+         DisplayRootViewFor<ShellViewModel>();
+      }
+      #endregion
+   }
+}
