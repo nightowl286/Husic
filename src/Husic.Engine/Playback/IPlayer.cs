184865ab54fb039d7f5c52ca3dbc38035c66a4c6
Nightowl286 <79215414+nightowl286@users.noreply.github.com>
2021-11-06 21:03:18 +0000

    Added the initial IPlayer and ISong interface definitions.

---
+++ src/Husic.Engine/Playback/IPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Husic.Engine.Playback
+{
+   public interface IPlayer
+   {
+      #region Properties
+      double Volume { get; set; }
+      TimeSpan Duration { get; }
+      TimeSpan Position { get; set; }
+      #endregion
+
+      #region Events
+      event Action PlaybackFinished;
+      event Action PlaybackStarted;
+      #endregion
+
+      #region Methods
+      void Load(Uri source);
+      void Stop();
+      void Play();
+      void Pause();
+      #endregion
+   }
+}
---
+++ src/Husic.Engine/Playback/ISong.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Husic.Engine.Playback
+{
+   public interface ISong
+   {
+      #region Properties
+      Uri Source { get; }
+      string Title { get; }
+      TimeSpan Duration { get; }
+      #endregion
+   }
+}
