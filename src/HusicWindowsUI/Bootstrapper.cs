c10f30a9046410f567aa5986f1f8f8338c7989e0
Nightowl286 <79215414+nightowl286@users.noreply.github.com>
2021-11-06 21:29:15 +0000

    Modified the MVVM bootstrapper to include an IoC
    Used the SimpleContainer provided by Caliburn.Micro as the IoC.
    Hooked up the overrides require to use the IoC and added 2 singletons
    one for a window manager and one for the event aggregator.

--- src/HusicWindowsUI/Bootstrapper.cs
+++ src/HusicWindowsUI/Bootstrapper.cs
@@ -3,6 +3,7 @@ using Husic.Windows.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,16 +12,39 @@ namespace Husic.Windows
 {
    internal class Bootstrapper : BootstrapperBase
    {
+      #region Private
+      private SimpleContainer _Container;
+      #endregion
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
       public Bootstrapper()
       {
          Initialize();
       }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
       #region Overrides
+      protected override void Configure()
+      {
+         _Container = new SimpleContainer();
+
+         _Container.Singleton<IWindowManager, WindowManager>();
+         _Container.Singleton<IEventAggregator, EventAggregator>();
+         _Container.Singleton<SimpleContainer>();
+
+         _Container.PerRequest<ShellViewModel>();
+      }
       protected override void OnStartup(object sender, StartupEventArgs e)
       {
          DisplayRootViewFor<ShellViewModel>();
       }
+      protected override IEnumerable<Assembly> SelectAssemblies() => new[] { Assembly.GetExecutingAssembly() };
+      #endregion
+
+      #region Overrides for container
+      protected override object GetInstance(Type service, string key) => _Container.GetInstance(service, key);
+      protected override IEnumerable<object> GetAllInstances(Type service) => _Container.GetAllInstances(service);
+      protected override void BuildUp(object instance) => _Container.BuildUp(instance);
       #endregion
    }
 }
