a604627892a54d86038eb916cf54031a51ad6e97
Nightowl286 <79215414+nightowl286@users.noreply.github.com>
2021-11-07 02:48:27 +0000

    Added an empty implementation of the dashboard

--- src/HusicWindowsUI/ViewModels/ShellViewModel.cs
+++ src/HusicWindowsUI/ViewModels/ShellViewModel.cs
@@ -3,11 +3,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Husic.Windows.ViewModels
 {
-   internal class ShellViewModel : PropertyChangedBase
+   internal class ShellViewModel : Conductor<object>
    {
+      #region Private
+      private readonly DashboardViewModel _Dashboard;
+      #endregion
+      public ShellViewModel(DashboardViewModel dashboard)
+      {
+         _Dashboard = dashboard;
+
+         ActivateItemAsync(_Dashboard);
+
+      }
+
    }
 }
