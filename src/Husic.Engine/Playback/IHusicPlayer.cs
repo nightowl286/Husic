4368929d4298fe448af4fe036f5380227fb685eb
Nightowl286 <79215414+nightowl286@users.noreply.github.com>
2021-11-07 03:05:59 +0000

    Added the interface for the HusicPlayer as well as the implementation.
    Added the IHusicPlayer interface and the implementation which is currently
    located in the WindowsUI project as it relies on Caliburn.Micro which the engine
    project should not rely on.

---
+++ src/Husic.Engine/Playback/IHusicPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Husic.Engine.Playback
+{
+   public interface IHusicPlayer : INotifyPropertyChanged
+   {
+      #region Properties
+      bool IsSongLoaded { get; }
+      bool IsPlaying { get; set; }
+      double Volume { get; set; }
+      bool IsMuted { get; set; }
+      TimeSpan Duration { get; }
+      TimeSpan Position { get; set; }
+      ISong CurrentlyPlaying { get; }
+      #endregion
+
+      #region Methods
+      void Play(ISong song);
+      void Play();
+      void Pause();
+      #endregion
+   }
+}
