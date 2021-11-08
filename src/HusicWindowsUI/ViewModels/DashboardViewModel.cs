28e64de1fb2b93bffb6891769cfc0c179e30b14a
Nightowl286 <79215414+nightowl286@users.noreply.github.com>
2021-11-08 07:34:50 +0000

    Updated the husic player and the dashboard to properly update and display the position and duration information.

--- src/HusicWindowsUI/ViewModels/DashboardViewModel.cs
+++ src/HusicWindowsUI/ViewModels/DashboardViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using Caliburn.Micro;
+using Husic.Engine.DataAccess;
+using Husic.Engine.Playback;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,54 @@ namespace Husic.Windows.ViewModels
 {
    internal class DashboardViewModel : Conductor<object>
    {
+      #region Private
+      private readonly IHusicPlayer _Player;
+      #endregion
+
+      #region Properties
+      public double Volume
+      {
+         get => _Player.Volume;
+         set
+         {
+            _Player.Volume = value;
+            NotifyOfPropertyChange();
+         }
+      }
+      // TODO(Nightowl) - Create a value converter for Timespan <-> String/Double
+      public double Duration
+      {
+         get => _Player.Duration.TotalSeconds;
+      }
+      public double Position
+      {
+         get => _Player.Position.TotalSeconds;
+         set
+         {
+            _Player.Position = TimeSpan.FromSeconds(value);
+            NotifyOfPropertyChange();
+         }
+      }
+      public string? SongName => _Player.CurrentlyPlaying?.Name;
+      #endregion
+      public DashboardViewModel(IHusicPlayer player, IDataAccess access)
+      {
+         _Player = player;
+         player.PropertyChanged += Player_PropertyChanged;
+
+         access.GetSongs().ContinueWith(songs => player.Play(songs.Result.First()));
+         
+      }
+
+      #region Events
+      private void Player_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+      {
+         string? prop = e.PropertyName;
+         if (prop == nameof(Volume) || prop == nameof(Duration) || prop == nameof(Position))
+            NotifyOfPropertyChange(prop);
+         else if (prop == nameof(_Player.CurrentlyPlaying))
+            NotifyOfPropertyChange(nameof(SongName));
+      }
+      #endregion
    }
 }
