264ad542e6af258e2f91cb89576358f0a171a4b4
Nightowl286 <79215414+nightowl286@users.noreply.github.com>
2021-11-12 05:03:32 +0000

    Added interfaces for the play queue
    Added the basic interfaces needed for the play queue
    implementations, no support for repeat or shuffle as of yet,
    some structures have been kept as interfaces even though they
    might not have logic as of yet as its likely that they might.

---
+++ src/Husic.Standard/Playback/Queue/IQueueSection.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Husic.Standard.Playback.Queue
+{
+   public interface IQueueSection : IQueueSectionBase<ISectionEntry>
+   {
+   }
+}
---
+++ src/Husic.Standard/Playback/Queue/ISectionEntry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Husic.Standard.Playback.Queue
+{
+   public interface ISectionEntry : INotifyPropertyChanged
+   {
+      #region Properties
+      ISong Song { get; }
+      int Index { get; set; }
+      int PlayIndex { get; set; }
+      #endregion
+   }
+}
