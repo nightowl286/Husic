d595d8f632de9150f576a71c5088800234953b18
Nightowl286 <79215414+nightowl286@users.noreply.github.com>
2021-11-15 09:19:05 +0000

    Linked the play queue to the bootstrapper.
    Linked the play queue implementation to the bootstrapper and
    linked up the play previous and play next buttons to the queue.

--- src/HusicWindowsUI/ViewModels/DashboardViewModel.cs
+++ src/HusicWindowsUI/ViewModels/DashboardViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Caliburn.Micro;
 using Husic.Standard.DataAccess;
 using Husic.Standard.Playback;
+using Husic.Standard.Playback.Queue;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,21 +14,25 @@ namespace Husic.Windows.ViewModels
    {
       #region Private
       private readonly IHusicPlayer _Player;
+      private readonly IPlayQueue _PlayQueue;
       #endregion
 
       #region Properties
       public IHusicPlayer Player => _Player;
       #endregion
-      public DashboardViewModel(IHusicPlayer player, IDataAccess access)
+      public DashboardViewModel(IHusicPlayer player, IDataAccess access, IPlayQueue playQueue)
       {
          _Player = player;
+         _PlayQueue = playQueue;
 
          //player.Volume = 0.5;
 
          player.PropertyChanged += Player_PropertyChanged;
+         playQueue.PropertyChanged += PlayQueue_PropertyChanged;
 
-         access.GetSongs().ContinueWith(songs => App.Current.Dispatcher.Invoke(() => player.Play(songs.Result.First())));
+         access.GetSongs().ContinueWith(songs => App.Current.Dispatcher.Invoke(() => AddSongs(songs.Result)));
       }
+
 
       #region Events
       private void Player_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
@@ -36,11 +41,19 @@ namespace Husic.Windows.ViewModels
          if (prop == nameof(Player.IsSongLoaded))
             NotifyOfPropertyChange(() => CanTogglePause);
       }
+      private void PlayQueue_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+      {
+         string? prop = e.PropertyName;
+         if (prop == nameof(_PlayQueue.NextSong))
+            NotifyOfPropertyChange(() => CanPlayNext);
+         if (prop == nameof(_PlayQueue.PreviousSong))
+            NotifyOfPropertyChange(() => CanPlayPrevious);
+      }
       #endregion
 
       #region Control buttons
-      public bool CanPlayPrevious => false;
-      public bool CanPlayNext => false;
+      public bool CanPlayPrevious => _PlayQueue.PreviousSong != null;
+      public bool CanPlayNext => _PlayQueue.NextSong != null;
       public bool CanTogglePause => Player.IsSongLoaded;
       public void TogglePause()
       {
@@ -48,6 +61,19 @@ namespace Husic.Windows.ViewModels
             Player.Pause();
          else
             Player.Play();
+      }
+      public void PlayPrevious() => _PlayQueue.MoveBackwards();
+      public void PlayNext() => _PlayQueue.MoveFowards();
+      #endregion
+
+      #region Methods
+      private void AddSongs(IEnumerable<ISong> songs)
+      {
+         foreach (ISong song in songs)
+         {
+            IUpNextEntry entry = new Engine.Playback.Queue.UpNextEntry() { Song = song };
+            _PlayQueue.UpNextSection.Entries.Add(entry);
+         }
       }
       #endregion
    }
