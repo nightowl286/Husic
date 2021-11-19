b3553ee7bc3785f75a17ac29747f5b3ad06fccab
Nightowl286 <79215414+nightowl286@users.noreply.github.com>
2021-11-19 14:21:10 +0000

    Added the DataCache interface and its implementation.

---
+++ src/Husic.Standard/DataAccess/IDataCache.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Husic.Standard.DataAccess
+{
+   public interface IDataCache<TKey, TData> where TData : class
+   {
+      #region Methods
+      void Delete(TKey key);
+      bool TryGet(TKey key, out TData data);
+      void Add(TKey key, TData data);
+      void Clear();
+      void CleanDead();
+      #endregion
+   }
+}
